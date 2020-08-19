a0411ee376b106b1094cd5ea2611c715230a885b
Max Zander <maxjacobzander@gmail.com>
2020-08-19 17:42:29 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 def square_array(array)
-  # your code here
+  i = 0
+  squared_array = [ ]
+  array.each do |number|
+    squared_array[i] = number ** 2
+    i += 1
+end
+  return squared_array
 end
