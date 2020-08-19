cdc4d046993662958857e3e03d5aa3a67e966089
Max Zander <maxjacobzander@gmail.com>
2020-08-19 17:37:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def square_array(array)
   i = 0
-  squared_array = []
+  squared_array = [ ]
   array.each do |number|
     squared_array = number ** 2
     i += 1
